a00735c8762a9b06eec9237feb774fd2f2c0769c
zarva6596 <zarva6596@gmail.com>
2023-12-02 02:21:15 +0200

    added breakpoints to styles and added classes for the margin and padding properties

---
+++ src/preview/classes/margin-padding.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    margin padding
+  </div>
+</template>
--- src/views/classes/index.vue
+++ src/views/classes/index.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
     Тут будуть універсальні класи якими можна буде робити багато речей.
+
+    <div class="ua-flex ua-mt-100">
+      <div class="ua-b ua-px-10">1</div>
+
+      <div class="ua-b ua-mx-100 ua-px-16 ua-py-20">2</div>
+
+      <div class="ua-b">3</div>
+
+      <div class="ua-b lg_ua-pr-200">4</div>
+    </div>
   </div>
 </template>
+
 <script setup lang="ts">
 </script>
+
+<style lang="scss">
+.classes {
+  display: flex;
+
+  &__sidebar {
+    flex: 0 0 300px;
+  }
+}
+</style>
